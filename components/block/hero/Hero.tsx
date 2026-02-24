f1c48d27e4e6c258a437e9820449071b8c14cad3
DmitryBatrakov <beowoolf61@gmail.com>
2026-02-24 21:07:54 +0200

    refactor(hero): update hero component structure and enhance text styling

--- components/block/hero/Hero.tsx
+++ components/block/hero/Hero.tsx
@@ -26,14 +26,14 @@ export const Hero = () => {
             <div className="min-h-screen flex flex-col items-center h-full justify-center relative px-5 pt-15 md:pt-30">
                 <div className="flex flex-col items-center justify-start gap-5 text-center max-w-2xl w-full mb-4 backdrop-blur-md px-1 py-10 rounded-xl bg-white/10">
                     <h1
-                        className="font-bold text-white tracking-wide wrap-break-word leading-tight text-[2.5rem] md:text-[3.25rem] lg:text-[4rem]"
+                        className="font-bold text-white tracking-wide wrap-break-word leading-tight text-[2.5rem] md:text-[3.25rem] lg:text-[4.5rem]"
                     >
                         A2B
                     </h1>
 
                     <div className="text-white text-center md:text-base max-w-xl leading-relaxed wrap-break-word">
-                        <h2 className="text-xl text-white md:text-2xl mb-1">{t("h2")}.</h2>
-                        <span className="text-white italic text-md md:text-lg max-w-1/2">
+                        <h2 className="text-xl text-white md:text-3xl mb-1">{t("h2")}</h2>
+                        <span className="text-white italic text-md md:text-xl max-w-1/2">
                             {t("h3")}
                         </span>
                     </div>
